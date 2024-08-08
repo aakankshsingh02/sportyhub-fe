339de2074349e5ac7e2f3261d3652ab446bc2ad7
Aakanksh Singh <aakanksh.singh@indiahealthlink.com>
2024-08-08 11:02:40 +0530

    Changed next js settings in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/reset-password/:token",
-        destination:
-          "https://sportyhub-be.onrender.com/api/auth/reset-password/:token", // Proxy to Backend
-      },
-    ];
-  },
   images: {
     domains: ["res.cloudinary.com"],
   },
